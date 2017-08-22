1402d344c553482a7e7469f0d941bd1643fa8379
Max Ramirez <ramirezmax1998@gmail.com>
2017-08-22 18:10:56 -0400

    Final Version

--- SoccerCoordinator/SoccerCoordinator.playground/Pages/SoccerCoordinator.xcplaygroundpage/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Pages/SoccerCoordinator.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+// Array collection of Dictionaries by name, heigh, experience, guardian
 var players: [[String : Any]] = [["name": "Joe Smith", "height": 42, "isExperienced": true, "guardian":"Jim and Jan Smith"],
                                  
                                  ["name": "Jill Tanner", "height": 36, "isExperienced": true, "guardian":"Clara Tanner "],
@@ -38,20 +38,97 @@ var players: [[String : Any]] = [["name": "Joe Smith", "height": 42, "isExperien
                                  
                                  ["name": "Herschel Krustofski", "height": 45,"isExperienced": true, "guardian":"Hyman and Rachel Krustofski"]]
 
-
+// The teams in their own array of Dictionaries
 var teamSharks: [[String: Any]] = []
 var teamDragons: [[String: Any]] = []
 var teamRaptors: [[String: Any]] = []
 
 
-func sort(by exp: Bool) -> Bool {
-    let isExperienced = Bool()
+// Iterating experience
+func ladsWithXp(in players: [[String : Any]]) -> Int {
+    
+    var ladsWithXp = 0
+  
     for player in players {
-        let experiencedPlayer = player["isExperienced"] as! Bool
-        if experiencedPlayer {
-            isExperienced
+         let hasExp = player["isExperienced"] as! Bool
+        if hasExp {
+            ladsWithXp += 1
         }
     }
-    return isExperienced
+    
+    
+    return ladsWithXp
 }
+// Iteration of players in order to sort by experience to own respective teams
+for player in players {
+    if let hasExp = player["isExperienced"] as? Bool {
+        // Sorting by less exp.
+        if hasExp == true {
+            if ladsWithXp(in: teamSharks) <= ladsWithXp(in: teamRaptors) && ladsWithXp(in: teamSharks) <= ladsWithXp(in: teamDragons) {
+                teamSharks.append(player)
+            } else if ladsWithXp(in: teamDragons) <= ladsWithXp(in: teamRaptors) && ladsWithXp(in: teamDragons) <= ladsWithXp(in: teamSharks) {
+                teamDragons.append(player)
+            } else if ladsWithXp(in: teamRaptors) <= ladsWithXp(in: teamSharks) && ladsWithXp(in: teamRaptors) <= ladsWithXp(in: teamDragons) {
+                teamRaptors.append(player)
+            }
+        } else {
+            if teamSharks.count <= teamRaptors.count && teamSharks.count <= teamDragons.count {
+                teamSharks.append(player)
+            
+        
+            
+            } else if teamDragons.count <= teamRaptors.count && teamDragons.count <= teamSharks.count {
+                teamDragons.append(player)
+            } else {
+                teamRaptors.append(player)
+            }
+        }
+    }
+}
+
+// Letters portion
+//Function that safely unwraps optionals
+func practiceLetter() -> [String] {
+let letters = [String]()
+for player in teamSharks {
+    if let name = player["name"], let guardian = player["guardian"] {
+        print("To \(guardian),\n \(name) has a practice seesion for team Sharks at March 17, 3pm.")
+    }
+
+}
+print("------------------------")
+for player in teamDragons {
+    if let name = player["name"], let guardian = player["guardian"] {
+        print("To \(guardian),\n \(name) has a practice seesion for team Dragons at March 17, 1pm.")
+    }
+}
+print("------------------------")
+for player in teamRaptors {
+    if let name = player["name"], let guardian = player["guardian"] {
+        print("To \(guardian),\n \(name) has a practice seesion for team Raptors at March 18, 1pm.")
+    }
+}
+
+return letters
+}
+//To show up in console
+practiceLetter()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
